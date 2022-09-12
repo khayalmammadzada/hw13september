424b3e67d5c4f7d034bbdf0e5943a7dd59b33d30
Khayal Mammadzada <khayalmammadzada@Khayals-MacBook-Air.local>
2022-09-13 03:39:24 +0400

    hw done

---
+++ Pronia_backend/ProniaWebsite/ProniaWebsite/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProniaWebsite.DAL;
+using ProniaWebsite.Models;
+//using ProniaWebsite.Models;
+
+namespace ProniaWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        ProniaContext _context;
+            public HomeController(ProniaContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Slider> sliders = _context.Sliders;
+            return View();
+        }
+
+        
+    }
+}
---
+++ Pronia_backend/ProniaWebsite/ProniaWebsite/DAL/ProniaContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using ProniaWebsite.Models;
+
+namespace ProniaWebsite.DAL
+{
+    public class ProniaContext: DbContext
+    {
+        public ProniaContext(DbContextOptions<ProniaContext> options): base(options)
+        {
+
+        }
+        public DbSet<Slider> Sliders { get; set; }
+
+    }
+}
